283b4eb7d74a1dfefec026d0967dc905952b0270
catherincantillano <Usuario@DESKTOP-4QLKQFA>
2021-02-08 13:28:16 -0600

    Agregando Referencias

--- QuieroPizza/QuieroPizza.Web/Controllers/ProductosController.cs
+++ QuieroPizza/QuieroPizza.Web/Controllers/ProductosController.cs
@@ -1,4 +1,6 @@
-﻿using QuieroPizza.Web.Models;
+﻿
+
+using QuieroPizza.BL;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,25 +14,12 @@ namespace QuieroPizza.Web.Controllers
         // GET: Productos
         public ActionResult Index()
         {
-            var producto1 = new ProductoModel();
-            producto1.Id = 1;
-            producto1.Descripcion = "Pizza 6 quesos";
+            var productoBL = new ProductosBL();
 
-            var producto2 = new ProductoModel();
-            producto2.Id = 2;
-            producto2.Descripcion = "Pizza 4 Estaciones ";
+            var listadeProductos = productoBL.ObtenerProductos();
 
-            var producto3 = new ProductoModel();
-            producto3.Id = 3;
-            producto3.Descripcion = "Pizza Jamon y Queso";
+            return View(listadeProductos);
 
-
-            var listadeproductos = new List<ProductoModel>();
-            listadeproductos.Add(producto1);
-            listadeproductos.Add(producto2);
-            listadeproductos.Add(producto3);
-
-            return View(listadeproductos);
         }
     }
 }
---
+++ QuieroPizza/QuieroPizza.Win/Form1.cs
@@ -0,0 +1,31 @@
+﻿using QuieroPizza.BL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuieroPizza.Win
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var productoBL = new ProductosBL();
+            var listadeProductos = productoBL.ObtenerProductos();
+            foreach (var productos in listadeProductos)
+            {
+                MessageBox.Show(productos.Descripcion);
+            }
+        }
+    }
+}
