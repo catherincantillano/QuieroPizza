db27da4b116aaeaff2882cc9c2fa08ce29248b35
Usuario <Usuario@DESKTOP-4QLKQFA>
2021-02-24 12:49:46 -0600

    Agregando el primer mantenimiento

---
+++ QuieroPizza/QuieroPiza.WebAdmin/Controllers/ProductosController.cs
@@ -0,0 +1,26 @@
+﻿using QuieroPizza.BL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QuieroPiza.WebAdmin.Controllers
+{
+    public class ProductosController : Controller
+    {
+        ProductosBL _productosBL;
+
+        public ProductosController()
+        {
+            _productosBL = new ProductosBL();
+        }
+        // GET: Productos
+        public ActionResult Index()
+        {
+            var listadeProductos = _productosBL.ObtenerProductos();
+
+            return View(listadeProductos);
+        }
+    }
+}
